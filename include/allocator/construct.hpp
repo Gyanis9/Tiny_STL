23369ede97c7891805887a331cbb73ccd9ad12ee
Gyanis <781680696@qq.com>
2025-06-04 19:48:27 +0800

    重构并添加注释

--- include/allocator/construct.hpp
+++ include/allocator/construct.hpp
@@ -1,56 +1,79 @@
 #ifndef TINY_STL_CONSTRUCT_HPP
 #define TINY_STL_CONSTRUCT_HPP
-
 #include <new>
-#include "../iiterator/iterator.hpp"
+#include "iterator/iterator.hpp"
 
 namespace Tiny
 {
+    /**
+     * @brief 在给定的地址上构造类型 T1 的对象，使用传入的 value 初始化
+     */
     template <typename T1, typename T2>
-    inline void construct(T1* p, const T2& value)
+    void construct(T1* p, const T2& value)
     {
-        new(p) T1(value);
+        new(p) T1(value); // 使用 placement new 进行对象构造
     }
 
+    /**
+     * @brief 销毁目标对象，调用其析构函数
+     */
     template <typename T>
-    inline void destroy(T* pointer)
+    void destroy(T* pointer)
     {
         pointer->~T();
     }
 
-
+    /**
+     * @brief 销毁范围内的对象，当类型没有显式析构函数时使用
+     */
     template <typename ForwardIterator>
-    inline void _destroy_aux(ForwardIterator first, ForwardIterator last, _false_type)
+    void destroy_aux(ForwardIterator first, ForwardIterator last, false_type)
     {
         for (; first != last; ++first)
-            destroy(&*first);
+            destroy(&*first); // 对范围中的每个元素调用 destroy
     }
 
+    /**
+     * @brief 销毁范围内的对象，当类型有显式析构函数时使用
+     */
     template <typename ForwardIterator>
-    inline void _destroy_aux(ForwardIterator first, ForwardIterator last, _true_type)
+    void destroy_aux(ForwardIterator, ForwardIterator, true_type)
     {
+        // No-op for types with trivial destructors
     }
 
+    /**
+     * @brief 销毁给定范围内的所有对象，决定是否需要显式析构
+     */
     template <typename ForwardIterator, typename T>
-    inline void _destroy(ForwardIterator first, ForwardIterator last, T*)
+    void destroy(ForwardIterator first, ForwardIterator last, T*)
     {
-        typedef typename _type_traits<T>::has_trivial_destructor trivial_destructor;
-        _destroy_aux(first, last, trivial_destructor());
+        typedef typename type_traits<T>::has_trivial_destructor trivial_destructor;
+        destroy_aux(first, last, trivial_destructor()); // 根据类型是否有显式析构函数选择合适的销毁方式
     }
 
+    /**
+     * @brief 销毁给定范围内的所有对象
+     */
     template <typename ForwardIterator>
-    inline void destroy(ForwardIterator first, ForwardIterator last)
+    void destroy(ForwardIterator first, ForwardIterator last)
     {
-        return _destroy(first, last, value_type(first));
+        return destroy(first, last, value_type(first)); // 传递范围内对象的类型
     }
 
+    /**
+     * @brief 特化销毁 char* 类型的空操作
+     */
     inline void destroy(char*, char*)
     {
     }
 
+    /**
+     * @brief 特化销毁 wchar_t* 类型的空操作
+     */
     inline void destroy(wchar_t*, wchar_t*)
     {
     }
 }
 
-#endif //TINY_STL_CONSTRUCT_HPP
+#endif
